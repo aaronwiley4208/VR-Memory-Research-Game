13133d7472e2599139eaed1791527950a3ae172e
aaronwiley4208 <35614460+aaronwiley4208@users.noreply.github.com>
2019-04-14 20:26:58 -0500

    Update CardShuffle.cs

--- VR Memory Research Project/Assets/Aaron/Scripts/CardShuffle.cs
+++ VR Memory Research Project/Assets/Aaron/Scripts/CardShuffle.cs
@@ -6,54 +6,75 @@ public class CardShuffle : MonoBehaviour {
 
 	public int patternSize = 6;
     public float lightTime = 1;
+    float timer = 0;
 	Transform[] cards;
     List<int> pattern = new List<int>();
-    float timer;
+    List<string> patNames = new List<string>();
+    int itertor = 0;
 
 	void Start () {
 		cards = GetComponentsInChildren<Transform>();
-		Transform tempGO;
-		for (int i = 1; i < cards.Length; i++) {
-			int rnd = Random.Range (1, cards.Length);
-			tempGO = cards [rnd];
-			cards [rnd] = cards [i];
-			cards [i] = tempGO;
-		}
-		
-		for(int i = 1; i <= 5; i++){//row 1
-			cards[i].localPosition = new Vector3 (-6 + (i * 2), 4, cards[i].localPosition.z);
-		}
-		for(int i = 1; i <= 5; i++){//row 2
-			cards[5+i].localPosition = new Vector3 (-6 + (i * 2), 2, cards[5+i].localPosition.z);
-		}
-		for(int i = 1; i <= 5; i++){//row 3
-			cards[10+i].localPosition = new Vector3 (-6 + (i * 2), 0, cards[10+i].localPosition.z);
-		}
-		for(int i = 1; i <= 5; i++){//row 4
-			cards[15+i].localPosition = new Vector3 (-6 + (i * 2), -2, cards[15+i].localPosition.z);
-		}
-		for(int i = 1; i <= 5; i++){//row 5
-			cards[20+i].localPosition = new Vector3 (-6 + (i * 2), -4, cards[20+i].localPosition.z);
-		}
+
+        Shuffle();
         CreatePat();
-        foreach (int str in pattern) {
+
+        /*foreach (int str in pattern) {
             print(str);
         }
-
-	}
+        foreach (string str in patNames)
+        {
+            print(str);
+        }*/
+    }
 
 	void Update () {
-
-		foreach (int num in pattern) {
-            timer = lightTime;
-            timer -= Time.deltaTime;
-            if(timer > 0)
-                cards[num].GetComponent<Light>().enabled = true;
-            if (timer < 0)
-                cards[num].GetComponent<Light>().enabled = false;
+        if (itertor < patternSize){
+            timer += Time.deltaTime;
+            if (timer >= lightTime){
+                cards[pattern[itertor]].GetComponent<Light>().enabled = true;
+                timer = 0;
+            }
+            timer += Time.deltaTime;
+            if (timer >= lightTime){
+                cards[pattern[itertor]].GetComponent<Light>().enabled = false;
+                timer = 0;
+                itertor++;
+            }
         }
 
 	}
+
+    void Shuffle() {
+        Transform tempGO;
+        for (int i = 1; i < cards.Length; i++)
+        {
+            int rnd = Random.Range(1, cards.Length);
+            tempGO = cards[rnd];
+            cards[rnd] = cards[i];
+            cards[i] = tempGO;
+        }
+
+        for (int i = 1; i <= 5; i++)
+        {//row 1
+            cards[i].localPosition = new Vector3(-6 + (i * 2), 4, cards[i].localPosition.z);
+        }
+        for (int i = 1; i <= 5; i++)
+        {//row 2
+            cards[5 + i].localPosition = new Vector3(-6 + (i * 2), 2, cards[5 + i].localPosition.z);
+        }
+        for (int i = 1; i <= 5; i++)
+        {//row 3
+            cards[10 + i].localPosition = new Vector3(-6 + (i * 2), 0, cards[10 + i].localPosition.z);
+        }
+        for (int i = 1; i <= 5; i++)
+        {//row 4
+            cards[15 + i].localPosition = new Vector3(-6 + (i * 2), -2, cards[15 + i].localPosition.z);
+        }
+        for (int i = 1; i <= 5; i++)
+        {//row 5
+            cards[20 + i].localPosition = new Vector3(-6 + (i * 2), -4, cards[20 + i].localPosition.z);
+        }
+    }
 
 	void CreatePat(){
         while(pattern.ToArray().Length != patternSize){ 
@@ -61,6 +82,11 @@ public class CardShuffle : MonoBehaviour {
             if (!pattern.Contains(ranNum))
                 pattern.Add(ranNum);                
 		}
-        
-	}
+        foreach (int num in pattern) {
+            patNames.Add(cards[num].GetComponent<Transform>().name);
+        }
+
+    }
+
+
 }
