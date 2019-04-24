374353406a3f7f1fac2134485860fdb0871cf0ec
Aaron <>
2019-04-24 13:56:07 -0500

    updated VR

--- VR Memory Research Project/Assets/Aaron/Scripts/RayScript.cs
+++ VR Memory Research Project/Assets/Aaron/Scripts/RayScript.cs
@@ -1,8 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class RayScript : MonoBehaviour
 {
+    public SteamVR_Action_Boolean grabPinch;
+
+    void Update() {
+
+        if (grabPinch.GetState(SteamVr_Input_Sources.Any)) {
+
+        }
+    }
     
 }
